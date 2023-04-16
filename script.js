c1573db0e68027ef2ee524946ebd057732fd1efe
HarenderSingh8055 <124910145+HarenderSingh8055@users.noreply.github.com>
2023-04-16 13:03:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+let button = document.getElementById("enterBtn");
+let status = document.getElementById("status");
+
+button.addEventListener("click",function(){
+	status.innerHTML = `<h1>Entered Metaverse</h1>`;
+})
+
